c9756ebc96fb99be65e1c153c7d1d4d0bd0c4773
Althaf <mgmalthaf@gmail.com>
2022-04-04 07:35:04 -0700

    initial update of neurolink responsible

---
+++ script.js
@@ -0,0 +1,27 @@
+
+/*
+ $("#menu-icon").click(function () {
+
+        $('#button_cont').modal('show');
+
+    });*/
+
+
+
+
+
+
+
+
+
+
+
+
+$('#menu-icon').on("click" , function(){
+    $('#button_cont').show()
+});
+
+
+$('#close_icon').on("click" , function(){
+    $('#button_cont').hide()
+});
